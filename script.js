8aa40391513b87a536258efcf9e24b1b9b159931
nisamozr <codeexplorkl10@gmail.com>
2021-09-24 16:44:12 +0530

    valide

--- script.js
+++ script.js
@@ -1,37 +1,165 @@
 // AOS.init();
-var names = document.getElementById('name').value;
-let email = document.getElementById('email').value;
-let place =document.getElementById('place').value;
-let phone = document.getElementById('phone').value;
-let gender = document.getElementById("gender").value;
-let employee = document.getElementById("employee").value;
-let qualification =document.getElementById("qualification").value;
-let collage = document.getElementById("collage").value;
-let graduateYear = document.getElementById("graduateYear").value;
-let aboutAs  =document.getElementById("aboutAs").value;
-let joinSps= document.getElementById("joinSps").value;
-let workHard = document.getElementById("workHard").value;
-let spsnews = document.getElementById("spsnews").value;
-let faqs = document.getElementById("faqs").value;
+var names = document.getElementById('name');
+var email = document.getElementById('email');
+var place = document.getElementById('place');
+let phone = document.getElementById('phone');
+let gender = document.getElementById('gender');
+let employee = document.getElementById('employee');
+let qualification =document.getElementById('qualification');
+let collage = document.getElementById('collage');
+let graduateYear = document.getElementById('graduateYear');
+let aboutAs  = document.getElementById('aboutAs');
+let joinSps = document.getElementById("joinSps");
+let workHard = document.getElementById('workHard');
+let spsnews = document.getElementById('spsnews');
+let faqs = document.getElementById('faqs');
 let form = document.getElementById("form");
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
 
 function validatform(){
-  
-    if(names == ''){
-        // alert("hhd");
-        // names.focus()
-        document.getElementById('name1').innerHTML= "This field is required";
-        return false;
+    if(names.value.length <= 5){
+
+        document.getElementById('name1').innerHTML= "Please enter at least 5 characters.";
+       
     }
-    if(place == ''){
-        document.getElementById('name5').innerHTML= "This field is requiredffffff";
+    if(names.value != ""){
+        document.getElementById('name1').innerHTML= "";
+       
+    }
+  
+    if(names.value == ""){
+
+        document.getElementById('name1').innerHTML= "This field is required";
+     
+        
+    }
+  
+    
+  
+    if(phone.value == ""){
+        document.getElementById('name2').innerHTML= "This field is required";
+        
+      
+    }
+    if(phone.value != ""){
+        document.getElementById('name2').innerHTML= "";
+        
+      
+    }
+    if(gender.value == ""){
+        document.getElementById('name3').innerHTML= "This field is required";
+        
+    }
+    if(gender.value != ""){
+        document.getElementById('name3').innerHTML= "";
+        
+    }
+    if(email.value == ''){
+        document.getElementById('name4').innerHTML= "This field is required";
+        
+    }
+    if(email.value != ''){
+        document.getElementById('name4').innerHTML= "";
+        
+    }
+    
+
+    if(email.value.match(mailformat)){
+        document.getElementById('name4').innerHTML= "Please enter a valid email address.";
+        
+    }
+    
+    if(place.value == ''){
+        document.getElementById('name5').innerHTML= "This field is required";
+        
+    }
+    if(place.value != ''){
+        document.getElementById('name5').innerHTML= "";
+        
+    }
+    if(employee.value == ''){
+        document.getElementById('name6').innerHTML= "This field is required";
+        
+    }
+    if(employee.value != ''){
+        document.getElementById('name6').innerHTML= "";
+        
+    }
+
+    if(qualification.value == ''){
+        document.getElementById('name7').innerHTML= "This field is required";
+       
+    }
+    if(qualification.value != ''){
+        document.getElementById('name7').innerHTML= "";
+       
+    }
+
+    if(collage.value == ''){
+        document.getElementById('name8').innerHTML= "This field is required";
+       
+    }
+    if(collage.value != ''){
+        document.getElementById('name8').innerHTML= "";
+       
+    }
+    if(graduateYear.value == ''){
+        document.getElementById('name9').innerHTML= "This field is required";
+       
+    }
+    if(graduateYear.value != ''){
+        document.getElementById('name9').innerHTML= "";
+       
+    }
+
+    if(aboutAs.value == ''){
+        document.getElementById('name10').innerHTML= "This field is required";
+       
+    }
+    if(aboutAs.value == ''){
+        document.getElementById('name10').innerHTML= "";
+       
+    }
+    if(joinSps.value == ''){
+        document.getElementById('name11').innerHTML= "This field is required";
+        
+    }
+    if(joinSps.value != ''){
+        document.getElementById('name11').innerHTML= "";
+        
+    }
+    
+    if(workHard.value == ''){
+        document.getElementById('name12').innerHTML= "This field is required";
+       
+    }
+    if(workHard.value != ''){
+        document.getElementById('name12').innerHTML= "";
+       
+    }
+
+
+    if(spsnews.value == ''){
+        document.getElementById('name13').innerHTML= "This field is required";
+       
+    }
+    if(spsnews.value != ''){
+        document.getElementById('name13').innerHTML= "";
+       
+    }
+    if(faqs.value == ''){
+        document.getElementById('name14').innerHTML= "This field is required";
+        
+    }
+    if(faqs.value == '' || spsnews.value == '' || workHard.value == '' || joinSps.value == ''|| aboutAs.value == '' || graduateYear.value == '' || collage.value == '' || employee.value == ''|| qualification.value == ''|| place.value == ''||  names.value == "" || email.value == '' || gender.value == "" || phone.value == ""  ){
         return false;
 
     }
     else{
         return true;
+     
     }
 
 }
