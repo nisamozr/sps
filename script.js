a628104e5b3d8f2e9560e22fca2182289fda10a6
nisamozr <codeexplorkl10@gmail.com>
2021-09-26 23:34:45 +0530

    message completed

--- script.js
+++ script.js
@@ -56,20 +56,22 @@ function validatform(){
         document.getElementById('name3').innerHTML= "";
         
     }
+    var pattern = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
     if(email.value == ''){
         document.getElementById('name4').innerHTML= "This field is required";
         
+    }else if(!pattern.test(email)){
+        document.getElementById('name4').innerHTML= "Please enter a valid email address.";
+        
     }
+  
     if(email.value != ''){
         document.getElementById('name4').innerHTML= "";
         
     }
     
-
-    if(email.value.match(mailformat)){
-        document.getElementById('name4').innerHTML= "Please enter a valid email address.";
-        
-    }
+    
+   
     
     if(place.value == ''){
         document.getElementById('name5').innerHTML= "This field is required";
@@ -162,5 +164,24 @@ function validatform(){
      
     }
 
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
